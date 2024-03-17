bd6744db3bd8a1898db1ca91e5c9fd36d7a3b97e
devshinon-br <rezendekeyla126@gmail.com>
2024-03-17 20:40:35 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const apiKey = 'api_key';
+    const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const movies = data.results;
+            const moviesContainer = document.getElementById('movies-container');
+
+            movies.forEach(movie => {
+                const movieElement = document.createElement('div');
+                movieElement.classList.add('movie');
+
+                const title = document.createElement('h2');
+                title.textContent = movie.title;
+
+                const posterPath = movie.poster_path;
+                const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
+                const poster = document.createElement('img');
+                poster.src = posterUrl;
+                poster.alt = movie.title;
+
+                movieElement.appendChild(title);
+                movieElement.appendChild(poster);
+
+                moviesContainer.appendChild(movieElement);
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao carregar filmes:', error);
+        });
+});
